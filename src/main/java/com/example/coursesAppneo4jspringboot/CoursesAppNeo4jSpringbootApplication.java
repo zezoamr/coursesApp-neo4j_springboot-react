da5bcb3db1411c38c42cf72c978388ca091ea09f
ziad amr shalaby <zezoamr@yandex.com>
2023-08-14 13:48:01 +0300

    Initial commit

---
+++ src/main/java/com/example/coursesAppneo4jspringboot/CoursesAppNeo4jSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursesAppneo4jspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesAppNeo4jSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesAppNeo4jSpringbootApplication.class, args);
+	}
+
+}
