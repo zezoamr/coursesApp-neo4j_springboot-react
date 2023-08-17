c744c6128a40259014b48c6b2a15dac893c28f12
ziad amr shalaby <zezoamr@yandex.com>
2023-08-18 00:20:38 +0300

    register component + routes + axios config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
+import Register from './components/register/Register';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/Register' element={<Register/>}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -5,9 +5,17 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate, NavLink, Link } from 'react-router-dom'
 
 
 const Header = () => {
+
+    const navigate = useNavigate();
+
+    const handleNav = (path) => {
+        navigate(path);
+    }
+
   return (
     <Navbar>
         <Container fluid>
@@ -20,9 +28,12 @@ const Header = () => {
                 style={{"maxHeight": "100px"}}
                 navbarScroll
             >
-                <Button variant='info' className='me-2'> Login </Button>
-                <Button variant='info' className='me-2'> Register </Button>
+                
             </Nav>
+            <>
+            <Button variant='info' className='me-2'> Login </Button>
+            <Button variant='info' className='me-2' onClick={() => handleNav("/Register")} >Register</Button>
+            </>
         </Container>
     </Navbar>
   )
